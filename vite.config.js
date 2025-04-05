5f77f1f2b2bd4527c470e8734ad70fc9a3027b60
Mohit Agrawal <mohitagrawal1305@gmail.com>
2025-04-05 18:29:06 +0530

    added gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const branch = process.env.BRANCH || 'main';
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${branch}/`,
+  base: `/${process.env.BRANCH || ""}/`,
 });
