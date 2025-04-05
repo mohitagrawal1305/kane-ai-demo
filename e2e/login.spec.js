9e697fea8d4144f4e71b5f3fae6f4ebace67241e
Mohit Agrawal <mohitagrawal1305@gmail.com>
2025-04-05 18:56:28 +0530

    added gh pages

--- e2e/login.spec.js
+++ e2e/login.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Login and increment counter', async ({ page }) => {
-  await page.goto('http://localhost:5173');
+  await page.goto('/');
   await page.fill('input[placeholder="Username"]', 'admin');
   await page.fill('input[placeholder="Password"]', 'password');
   await page.click('text=Login');
